bb1b3a2dd5504f33f538d30df57c53f7ebfa4475
Muhamed-Shillua <muhamedshillua303@gmail.com>
2026-04-24 19:45:20 +0300

    chore(config): add difficulty configuration mapping

---
+++ beginner /number-guessing-game/src/config/difficulty.js
@@ -0,0 +1,11 @@
+/**
+ * Difficulty Configuration
+ * ------------------------
+ * Centralized mapping between difficulty levels and attempts.
+ */
+
+export const difficultyMap = {
+  easy: 10,
+  medium: 5,
+  hard: 3,
+};
